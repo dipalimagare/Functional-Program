e608317d58e54f49be0ebd6fff0942724e95c1e0
ikalpesh <kalpeshmahajan90@gmail.com>
2021-07-29 17:31:26 +0530

    Functional Programs

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -8,11 +8,18 @@ public class EvenOrOdd {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a number : ");
 		int number = sc.nextInt();
+		sc.close();
 
-		if (number % 2 == 0) {
+		boolean result = isEven(number);
+		if (isEven(number)) {
 			System.out.println("Number is Even");
-		}else {
+		} else {
 			System.out.println("Number is Odd");
 		}
 	}
+
+	public static boolean isEven(int number) {
+		return (number % 2 == 0);
+	}
+
 }
